4b57e6d3256516db6323b59230cc3b83146ebfe5
EC2 Default User <ec2-user@ip-172-31-24-225.ap-northeast-1.compute.internal>
2023-07-14 21:16:06 +0000

    javascript_start

---
+++ app.js
@@ -0,0 +1,10 @@
+let orange = 100;
+let apple = 120;
+
+if(orange < apple) {
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごは同じ値段');
+} else{
+  alert('みかんの値段はりんごより高い');
+}
